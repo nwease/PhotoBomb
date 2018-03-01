d3147c9e049e99161ad45dcb414690c4b2b930e0
Nicholas Wease <skynet60@outlook.com>
2018-03-01 11:42:59 +0100

    added new routes and improved the UI

--- routes/index.js
+++ routes/index.js
@@ -23,17 +23,57 @@ router.get('/:username', (req, res) => {
     .set('Accept', 'application/json')
     .end((err, response) => {
       if (err) {
-        res.json({
-          confirmation: 'fail',
-          message: err.message
-        })
+        const data = {
+          message: err.message || 'Check your spelling!'
+        }
+        res.render('error', data)
         return
       }
 
       res.render('index', response.body)
+    })
+})
+
+router.get('/:username/:postcode', (req, res) => {
+  const username = req.params.username
+  const postcode = req.params.postcode
+
+  const instagramAPI = 'https://www.instagram.com/' + username + '/?__a=1'
+
+  superagent
+    .get(instagramAPI)
+    .query(null)
+    .set('Accept', 'application/json')
+    .end((err, response) => {
+      if (err) {
+        const data = {
+          message: err.message || 'Check your spelling!'
+        }
+        res.render('error', data)
+        return
+      }
+
+      const posts = response.body.user.media.nodes
+      let selectedPost = null
+
+      for (let i = 0; i < posts.length; i++) {
+        const post = posts[i]
+        if (post.code == postcode) {
+          selectedPost = post
+          break
+        }
+      }
+
+      if (selectedPost == null) {
+        res.render('error', { message: 'Post not found!' })
+        return
+      }
+
+      res.render('post', selectedPost)
+
       //res.json({
       //  confirmation: 'success',
-      //  data: response.body
+      //  data: selectedPost
       //})
     })
 })
